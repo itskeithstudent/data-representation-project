fcf6fa3809f346d5d254ce67a34fa405fa1739e8
itskeithstudent <G00387816@gmit.ie>
2021-11-17 23:46:09 +0000

    Update app.py to connect to mysql server, send simple query and return response via GET API call

--- Server/app.py
+++ Server/app.py
@@ -2,12 +2,30 @@
 from flask import Flask, jsonify,  request, abort, make_response
 from flask.templating import render_template
 from flask_cors import CORS
+
+#import pymysql
+import mysql.connector
+
 app = Flask(__name__)
+
+#initialise connection
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="G00387816_DataRepProject"
+)
+#create cursor object
+cursor = conn.cursor()
 
 @app.route('/', methods=['GET'])
 def test():
     print("Hello Terminal")
-    return "Hello Curl or Browser"
+    cursor.execute("SELECT * FROM MOVIES;")
+    query_result = cursor.fetchall()
+    for row in query_result:
+        print(row)
+    return jsonify(data=query_result)
 
 if __name__ == '__main__':
     app.run(debug=True)
