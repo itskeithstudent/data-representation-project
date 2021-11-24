9751945d2f4243c994fe987a46a02ae4ffbe451c
itskeithstudent <G00387816@gmit.ie>
2021-11-24 00:08:35 +0000

    Add POST method for update_film function, add broken update query to test error handling

--- Server/app.py
+++ Server/app.py
@@ -1,11 +1,11 @@
 #!flask/bin/python
+from os import error
 from flask import Flask, jsonify,  request, abort, make_response
 from flask.templating import render_template
 from flask_cors import CORS
 
 #import pymysql
 import mysql.connector
-
 
 import queries
 
@@ -26,6 +26,7 @@ cursor = conn.cursor()
 '''
 @app.route('/', methods=['GET'])
 def test():
+    #cursor.execute(queries.select_all_movie_plus_rating)
     cursor.execute(queries.select_all_movie)
     query_result = cursor.fetchall()
     column_names = [i[0] for i in cursor.description]
@@ -52,15 +53,41 @@ def add_film():
         movie_title = None #strangely I've decided a movie does not necessarily need a name
     if "RatingID" in request.json:
         rating_id = request.json['RatingID']
-        abort(403) #movie must have a rating
     else:
-        rating_id = None
+        abort(403) #movie must have a rating
     cursor.execute(queries.insert_movie, (movie_id, movie_title, rating_id))
     return request.json
 
 '''
     TODO - Add POST and DELETE methods for different functions and add html functionality
 '''
+@app.route('/', methods=['PUT'])
+def update_film():
+    if not request.json:
+        abort(400)
+    if "MovieID" in request.json:
+        movie_id = request.json['MovieID']
+    else:
+        abort(403) #movie must have an identifier
+    if "Title" in request.json:
+        movie_title = request.json['Title']
+    else:
+        movie_title = None #strangely I've decided a movie does not necessarily need a name
+    if "RatingID" in request.json:
+        rating_id = request.json['RatingID']
+    else:
+        abort(403) #movie must have a rating
+    try:
+        cursor.execute(queries.update_movie, ( movie_title, rating_id, movie_id))
+    except mysql.connector.Error as e:
+        print("Hit some issue")
+        print(e)
+        print(cursor.statement)
+    except Exception as e:
+        print("Hit some other weirder issue")
+        print(e)
+    return request.json
+
 
 
 if __name__ == '__main__':
--- Server/queries.py
+++ Server/queries.py
@@ -4,3 +4,12 @@ insert_movie = '''INSERT INTO MOVIES
 VALUES(%s, %s, %s);'''
 
 select_all_movie = "SELECT * FROM MOVIES;"
+
+select_all_movie_plus_rating = '''SELECT m.MovieID, m.Title, r.RatingID, r.Rating
+FROM MOVIES m INNER JOIN RATING r ON m.RatingID = r.RatingID;'''
+
+update_movie = '''UPDATE MOVIES
+SET
+Title=%s
+RatingID=%s
+WHERE MovieID=%s;'''
