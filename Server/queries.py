ca825be11527e2e92b64b8ac3cedd68be0186023
itskeithstudent <G00387816@gmit.ie>
2021-12-03 00:16:05 +0000

    Update queries to be correct case for table names as when querying against docker container for mysql will hit error

--- Server/queries.py
+++ Server/queries.py
@@ -1,20 +1,20 @@
 #insertion query for new movie id, movie name and 
-insert_movie = '''INSERT INTO MOVIES
+insert_movie = '''INSERT INTO Movies
 (MovieID, Title, RatingID)
 VALUES(%s, %s, %s);'''
 
-select_all_movie = "SELECT * FROM MOVIES;"
+select_all_movie = "SELECT * FROM Movies;"
 
 select_all_movie_plus_rating = '''SELECT m.MovieID, m.Title, r.RatingID, r.Rating
-FROM MOVIES m INNER JOIN RATING r ON m.RatingID = r.RatingID;'''
+FROM Movies m INNER JOIN Rating r ON m.RatingID = r.RatingID;'''
 
-update_movie = '''UPDATE MOVIES
+update_movie = '''UPDATE Movies
 SET
 Title=%s,
 RatingID=%s
 WHERE MovieID=%s;'''
 
-delete_movie = '''DELETE FROM MOVIES
+delete_movie = '''DELETE FROM Movies
 WHERE MovieID=%s;'''
 
-select_all_ratings = "SELECT * FROM RATING;"
+select_all_ratings = "SELECT * FROM Rating;"
