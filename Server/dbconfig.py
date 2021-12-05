323657c826cc98a5a4bffb02b97c53a3f15892f0
itskeithstudent <G00387816@gmit.ie>
2021-12-05 16:59:59 +0000

    Make changes for deployment to pythonanywhere

--- Server/dbconfig.py
+++ Server/dbconfig.py
@@ -1,8 +1,9 @@
 import os
 
-environment_check = os.environ.get('DOCKER_ENV', False)
+environment_check_docker = os.environ.get('DOCKER_ENV', False)
+environment_check_pythonanywhere = os.environ.get('PYTHONANYWHERE_SITE', False)
 
-if environment_check:
+if environment_check_docker:
     mysql = {
         'host':'db',
         'user':'root',
@@ -10,6 +11,16 @@ if environment_check:
         'database':'G00387816_DataRepProject',
         'default_pool_name':'movie_conn_pool',
         'default_pool_size':10,
+        'port':3306
+    }
+elif environment_check_pythonanywhere:
+    mysql = {
+        'host':'G00387816.mysql.pythonanywhere-services.com',
+        'user':'G00387816',
+        'password':'rootroot',
+        'database':'G00387816$G00387816_DataRepProject',
+        'default_pool_name':'movie_conn_pool',
+        'default_pool_size':3,
         'port':3306
     }
 else:
